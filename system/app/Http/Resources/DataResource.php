2e4f1bfbcb773b7417cd1eaef4b93dee0d1619d9
Widi Arrohman <widiarrohman1234@gmail.com>
2023-02-22 23:13:30 +0700

    Create DataResource.php

---
+++ system/app/Http/Resources/DataResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DataResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+
+
+}
